5a98180b544702f366dc66ecf66b1813ce452968
placebo <placebojones07@gmail.com>
2017-09-06 12:15:29 -0700

    Add files via upload

---
+++ Game of Thrones/resources/js/scripts.js
@@ -0,0 +1,4 @@
+ $('.js--scroll-to-houses').click(function(){ /* Selecting this class in the HTML code */
+       $('html, body').animate({scrollTop: $('.js--section-houses').offset().top}, 1000); /* This is what happens to the HTML code, the 'animation of the page' */ 
+        
+    });
