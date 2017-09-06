ee86d9b8299c665fec409433e29c5ef8f0c31c34
placebo <placebojones07@gmail.com>
2017-09-06 14:57:50 -0700

    Add files via upload

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -2,3 +2,9 @@
        $('html, body').animate({scrollTop: $('.js--section-houses').offset().top}, 1000); /* This is what happens to the HTML code, the 'animation of the page' */ 
         
     });
+
+
+ $('.js--scroll-to-history').click(function(){ /* Selecting this class in the HTML code */
+       $('html, body').animate({scrollTop: $('.js--section-history').offset().top}, 1000); /* This is what happens to the HTML code, the 'animation of the page' */ 
+        
+    });
